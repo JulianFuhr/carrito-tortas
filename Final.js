f93b4536c0ed55f9e86ad3a4c2f7608c15fa4689
Luks796 <47389958+Luks796@users.noreply.github.com>
2022-06-22 21:05:03 -0300

    Correccion de formato y limpieza de codigo

--- Final.js
+++ Final.js
@@ -1,72 +1,62 @@
-const cards = document.getElementById('cards')
-const items = document.getElementById('items')
-const footer = document.getElementById('footer')
-const templateCard = document.getElementById('template-card').content
-const templateFooter = document.getElementById('template-footer').content
-const templateCarrito = document.getElementById('template-carrito').content
-const fragment = document.createDocumentFragment()
+const cards = document.getElementById("cards");
+const items = document.getElementById("items");
+const footer = document.getElementById("footer");
+const templateCard = document.getElementById("template-card").content;
+const templateFooter = document.getElementById("template-footer").content;
+const templateCarrito = document.getElementById("template-carrito").content;
+const fragment = document.createDocumentFragment();
 
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+  if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    pintarCarrito();
+  }
+});
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetchData()
-    if (localStorage.getItem('carrito')) {
-        carrito = JSON.parse(localStorage.getItem('carrito'))
-        pintarCarrito()
-    }
-})
+cards.addEventListener("click", (e) => {
+  addCarrito(e);
+});
 
-cards.addEventListener('click', e => {
-    addCarrito(e)
-})
-
-items.addEventListener('click', e => {
-    btnAccion(e)
-})
+items.addEventListener("click", (e) => {
+  btnAccion(e);
+});
 
 const fetchData = async () => {
-    try {
-        const res = await fetch('api.json');
-        const data = await res.json();
-        // console.log(data);
-        pintarCards(data)
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const res = await fetch("api.json");
+    const data = await res.json();
+    pintarCards(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const pintarCards = data => {
-    // console.log(data);
-    data.forEach(producto => {
-        templateCard.querySelector('h5').textContent = producto.title
-        templateCard.querySelector('p').textContent = producto.precio
-        templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl)
-        templateCard.querySelector('.btn-dark').dataset.id = producto.id
-        const clone = templateCard.cloneNode(true)
-        fragment.appendChild(clone)
-    });
-    cards.appendChild(fragment)
+const pintarCards = (data) => {
+  data.forEach((producto) => {
+    templateCard.querySelector("h5").textContent = producto.title;
+    templateCard.querySelector("p").textContent = producto.precio;
+    templateCard
+      .querySelector("img")
+      .setAttribute("src", producto.thumbnailUrl);
+    templateCard.querySelector(".btn-dark").dataset.id = producto.id;
+    const clone = templateCard.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  cards.appendChild(fragment);
+};
 
-}
-const addCarrito = e => {
-    //    console.log(e.target);
-    //    console.log(e.target.classList.contains('btn-dark'));
-    if (e.target.classList.contains('btn-dark')) {
-        setCarrito(e.target.parentElement)
-    }
-    e.stopPropagation()
-}
-
-
-
-
-
-
-
+const addCarrito = (e) => {
+  if (e.target.classList.contains("btn-dark")) {
+    setCarrito(e.target.parentElement);
+  }
+  e.stopPropagation();
+};
 
 Swal.fire({
-    title: 'Bienvenid@s a Carolas Tortas ',
-  })     
+  title: "Bienvenid@s a Carolas Tortas ",
+});
 
-function alerta(){
-    alert("Muchas gracias por su compra")
+function alerta() {
+  alert("Muchas gracias por su compra");
 }
